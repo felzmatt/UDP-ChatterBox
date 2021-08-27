e0ba8cd9dcf58e8f02060a57a831c7fbb585ed67
Matteo <felmat23@gmail.com>
2021-08-27 19:59:29 +0200

    working on client-side

---
+++ client.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#include "list.h"
+#include "server.h"
+
+#define RECEIVER_THREAD 0
+#define INTERACTIVE_THREAD 1
+
+#define BLACK  "\033[0;30m\0"
+#define RED  "\033[0;31m\0"
+#define GREEN   "\033[0;32m\0"
+#define YELLOW   "\033[0;33m\0"
+#define BLUE   "\033[0;34m\0"
+#define MAGENTA   "\033[0;35m\0"
+#define CYAN   "\033[0;36m\0"
+#define WHITE  "\033[0;37m\0"
+#define END_COLOR "\33[0m"
+
+#define PRETTY_WELCOME  "Client process to interact with UDP-ChatterBox\0"
+
+
+/**
+ * ------------- RETURN CODES -------------------
+ */
+
+// NEWUSER
+
+#define LOGIN_SUCCESS
+#define LOGIN_FAIL
+
+/**
+ * ----------- COMMAND INDEXING --------------------
+ */
+
+// NOT LOGGED
+
+#define NEWUSER_COMMAND 0
+#define LOGIN_COMMAND 1
+
+// LOGGED
+
+/**
+ * ---------------------------------------------------
+ */
+
+
+/**
+ * ----------- COMMANDS LABELS ----------------------
+ */
+
+// NOT LOGGED
+
+#define NEWUSER_LABEL "[0] NEWUSER\0"
+#define LOGIN_LABEL "[1] LOGIN\0"
+
+
+// LOGGED
+#define MESSAGE_LABEL "[2] MESSAGE\0"
+
+
+/**
+ * --------------------------------------------------
+ */
+
+
+/**
+ * ------------ COMMANDS PROCEDURES ------------------
+ */
+
+// NOT LOGGED
+
+int newuser_command();
